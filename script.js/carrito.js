4c7741fecd6e52d8884aa5cf389652944c556a11
sofialau17 <munozsofia751@gmail.com>
2023-10-17 14:29:25 -0500

    add file

--- script.js/carrito.js
+++ script.js/carrito.js
@@ -1,11 +1,24 @@
 let boxPlus= document.getElementById("boxPlus");
 let boxMinus= document.getElementById("boxMinus");
 let boxUnidades= document.getElementById("boxUnidades");
+let subTotal = document.getElementById ("subTotal");
+const valorUnidad =  50000;
+const valorUnidadStr = Intl.NumberFormat("de-De").format(valorUnidad);
+document.getElementById ("precioUnidad").innerHTML= "$" + valorUnidadStr;
 
 boxPlus.addEventListener("click", function(){
     let auxUnidades = parseInt(boxUnidades.innerHTML);
     if (auxUnidades < 10){
        boxUnidades.innerHTML = (auxUnidades +1).toString();
+       let valorSubtotal= parseInt(boxUnidades.innerHTML) * valorUnidad;
+       subTotal.innerHTML = Intl.NumberFormat ("de-De").format (valorSubtotal);
+       if (boxUnidades.innerHTML == 1){
+        document.getElementById ("cantProductos").innerHTML =
+        boxUnidades.innerHTML + "Producto";
+       }
+       else {
+        document.getElementById("cantProductos").innerHTML = boxUnidades.innerHTML + "Productos";
+       } 
     }
 });
 
@@ -13,5 +26,16 @@ boxMinus.addEventListener("click", function(){
     let auxUnidades = parseInt(boxUnidades.innerHTML);
     if (auxUnidades > 0){
        boxUnidades.innerHTML = (auxUnidades -1).toString();
+       let valorSubtotal= parseInt(boxUnidades.innerHTML) * valorUnidad;
+       subTotal.innerHTML = Intl.NumberFormat ("de-De").format (valorSubtotal);
+       if (boxUnidades.innerHTML == 1){
+        document.getElementById ("cantProductos").innerHTML =
+        boxUnidades.innerHTML + "Producto";
+       }
+       else {
+        document.getElementById("cantProductos").innerHTML = boxUnidades.innerHTML + "Productos";
+       } 
+
     }
 });
+
