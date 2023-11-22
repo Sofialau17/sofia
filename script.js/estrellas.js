a4710e2029ad389d2a13a4ed204213c0a447cc52
sofialau17 <munozsofia751@gmail.com>
2023-11-22 13:20:40 -0500

    add file

--- script.js/estrellas.js
+++ script.js/estrellas.js
@@ -1,11 +1,32 @@
 let inputBusqueda = document.getElementById("inputBusqueda");
-let btnGraficar = document.getElementById("btnGraficar");
-let boxEstrella = document.getElementById("boxEstrellas");
+let btngraficar = document.getElementById("btngraficar");
 
 
-btnGraficar.addEventListener("click", function(){
-    let boxEstrella = document.createAttribute("i");
-    boxEstrella.appendChild(boxEstrella);
-    boxEstrella.setAtrtibute("class", "fa-solid fa-star");
+let boxEstrellas = document.getElementById("boxEstrellas");
+ let boxEstrella1 = document.createElement("i");
+ let boxEstrella2 = document.createElement("i");
+ let boxEstrella3 = document.createElement("i");
+ let boxEstrella4 = document.createElement("i");
+ let boxEstrella5 = document.createElement("i");
+ 
+window.addEventListener("load",function(){
+    boxEstrellas.appendChild(boxEstrella1);
+    boxEstrellas.appendChild(boxEstrella2);
+    boxEstrellas.appendChild(boxEstrella3);
+    boxEstrellas.appendChild(boxEstrella4);
+    boxEstrellas.appendChild(boxEstrella5);
+})
+btngraficar.addEventListener("click", function (){
+    let calificacion = parseInt(inputBusqueda.value);
+    if(calificacion >= 0 && calificacion <= 0.2){
+        boxEstrella1.setAttribute("class","fa-regular fa-star");
+        boxEstrella2.setAttribute("class","fa-regular fa-star");
+        boxEstrella3.setAttribute("class","fa-regular fa-star");
+        boxEstrella4.setAttribute("class","fa-regular fa-star");
+        boxEstrella5.setAttribute("class","fa-regular fa-star");
+    }
+   
+    /* boxEstrellas.appendChild(boxEstrella);
+    boxEstrella.setAttribute("class", "fa-solid fa-star"); */
 
 });
